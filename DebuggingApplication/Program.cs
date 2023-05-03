15450aba7709fe599e670e64ac1fba379baf4edf
rider <jil.p@simformsolutions.com>
2023-05-03 09:32:32 +0530

    add: DebuggingApplication practical

---
+++ DebuggingApplication/Program.cs
@@ -0,0 +1,11 @@
+﻿class DebugApp
+{
+    public static void Main(String[] args)
+    {
+        string? userResponse = Console.ReadLine();
+        if(Convert.ToBoolean(int.Parse(userResponse)))
+        {
+            Console.WriteLine("The entered value is a integer");
+        }
+    }
+}
